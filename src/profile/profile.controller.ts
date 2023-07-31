e018be09a7bcb1291c76d98c1e2554d12924d09d
lexxluey <biggestluey@gmail.com>
2023-07-31 10:46:26 +0100

    updated profile creation endpoint and docs

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -9,8 +9,12 @@ import {
 } from '@nestjs/common';
 import { AuthenticatedGuard } from 'src/auth/authenticated.guard';
 import { ProfileService } from './profile.service';
-import { ProfileCreationDto } from './profile.types';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
+import {
+  ProfileCreationDto,
+  ProfileCreationResponseDto,
+} from './profile.types';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { JwtGuard } from 'src/auth/guard';
 
 @ApiTags('Profile')
 @Controller('profile')
@@ -23,61 +27,21 @@ export class ProfileController {
     return await this.profileService.fetchUserProfileById({ id: userId });
   }
 
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        phone_number: { type: 'string' },
-        occupation: { type: 'string' },
-        birthday: { type: 'string', format: 'date-time' },
-        marital_status: { type: 'string' },
-        marriage_anniversary: { type: `string`, format: 'date-time' },
-        is_church_member: { type: 'boolean' },
-        church_join_date: { type: `string`, format: 'date-time' },
-        growth_track_completed: { type: 'boolean' },
-        is_tither: { type: 'boolean' },
-        is_partner: { type: 'boolean' },
-        payment_interval: { type: 'string' },
-        church_centre_id: { type: 'number', format: 'int32' },
-        home_cell_id: { type: 'number', format: 'int32' },
-        colony_id: { type: 'number', format: 'int32' },
-        roles_and_responsibilities: {
-          type: 'array',
-          items: { type: 'number', format: 'int32' },
-        },
-        service_team: {
-          type: 'array',
-          items: { type: 'number', format: 'int32' },
-        },
-        partnered_arms: {
-          type: 'array',
-          items: { type: 'number', format: 'int32' },
-        },
-        dependents: {
-          type: 'array',
-          items: {
-            title: 'Dependent',
-            type: 'object',
-            properties: {
-              first_name: { type: 'string' },
-              last_name: { type: 'string' },
-              birthday: { type: 'string', format: 'date-time' },
-              allergies: { type: 'string' },
-              emergency_contact: { type: 'string' },
-            },
-          },
-        },
-      },
-    },
+  @ApiBody({ type: ProfileCreationDto })
+  @ApiResponse({
+    status: 201,
+    description: 'The profile has been successfully created.',
+    type: ProfileCreationResponseDto,
   })
   @Post('/create')
-  @UseGuards(AuthenticatedGuard)
+  @UseGuards(JwtGuard)
   async createProfile(
     @Req()
     req: any,
     @Body() createProfileDto: ProfileCreationDto,
   ): Promise<any> {
     const userId = req.user.id;
+    console.log(userId);
     return await this.profileService.createUserProfile(
       createProfileDto,
       userId,
